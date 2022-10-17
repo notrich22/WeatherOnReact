5302d3964baa0bd063895f4eca77bc662d6cdb2a
notrich22 <notrich22@mail.ru>
2022-10-17 12:52:34 +0200

    1. Commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import weatherHandler from "./weatherHandler";
+import './Weather.css';
+class Weather extends React.Component{
+  
+  
+
+  render(){
+    return <div classNameName="App">
+      <section className="contentPanel">
+        <div className="gridContainer">
+            <div className="weatherForm">
+                <h2>Enter <span>city</span> for the weather broadcast</h2>
+                <div id="WeatherContainer">
+                    <div className="formControl">
+                        <input id ="chosenCity" type="text" defaultValue="Moscow" autoComplete="off"/>
+                    </div>
+                    <input id ="submitBtn" className="forecastBtn" onClick={weatherHandler} type="submit" defaultValue=""/>
+                </div>
+            </div>
+    
+            <div className="forecastResult">
+                <ul>
+                    <li id="city"></li>
+                    <hr/>
+                    <li id="state"></li>
+                    <li><img id="image"/></li>
+                    <li id="temperature"></li>
+                    <li id="feelsLike"></li>
+                    <li id="wind"></li>
+                    <li id="humidity"></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    </div>
+  }
+}
+
+export default Weather;
---
+++ src/weatherHandler.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+async function getWeather(city) {
+    const API_key = "1195410e4ae2d05ce024d37aa18407de";
+    const url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+API_key;
+    console.log(url);
+    const response = await axios.get(url).catch(async(error) =>{
+        console.log(error.toJSON());
+        return undefined;
+    })
+    return response;
+}
+export async function  WeatherBtnPressed(){
+    let city = document.getElementById("city");
+    if(document.getElementById("chosenCity").value == ""){
+        alert("Enter city");
+        return;
+    }
+    let state = document.getElementById("state");
+    let temperature = document.getElementById("temperature");
+    let feelsLike = document.getElementById("feelsLike");
+    let wind = document.getElementById("wind");
+    let humidity = document.getElementById("humidity");
+    let image = document.getElementById("image");
+    
+    let weather = await getWeather(document.getElementById("chosenCity").value);
+
+    if(weather == undefined){
+        alert("Something went wrong!");
+        return;
+    }
+    console.log(weather);
+    image.src = "https://api.openweathermap.org/img/w/" + weather.data.weather[0].icon + ".png";
+    city.innerText = document.getElementById("chosenCity").value;
+    state.innerText = weather.data.weather[0].description;
+    temperature.innerHTML = "Temperature: " + Math.round(weather.data.main.temp - 273.15) + "°C";
+    feelsLike.innerText = "Feels like: " + Math.round(weather.data.main.temp - 273.15) + "°C";
+    wind.innerText = "Wind: " + Math.round(weather.data.wind.speed) + " м/с";
+    humidity.innerText = "Humidity: " + weather.data.main.humidity + " %";
+
+}
+window.addEventListener("load", WeatherBtnPressed);
+export default WeatherBtnPressed;
