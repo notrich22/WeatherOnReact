71dbf1a1558d839f9c5df28c6b4d9135e302b25a
notrich22 <notrich22@mail.ru>
2022-10-21 10:32:08 +0200

    commit

--- App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Weather from './Weather';
-
-test('renders learn react link', () => {
-  render(<Weather />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
