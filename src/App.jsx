e3d5965a6f436ae9e0b9e02444e7a91a71f80b3b
notrich22 <notrich22@mail.ru>
2022-10-24 10:41:05 +0200

    Class reworked

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,84 @@
 import React from 'react';
-import weatherHandler from "./weatherHandler";
+import axios from 'axios';
 import './Weather.css';
 class Weather extends React.Component{
-  render(){
-    return <div classNameName="App">
-      <section className="contentPanel">
-        <div className="gridContainer">
-            <div className="weatherForm">
-                <h2>Enter <span>city</span> for the weather forecast</h2>
-                <div id="WeatherContainer">
-                    <div className="formControl">
-                        <input id ="chosenCity" type="text" defaultValue="Moscow" autoComplete="off"/>
-                    </div>
-                    <input id ="submitBtn" className="forecastBtn" onClick={weatherHandler} type="button" defaultValue="See forecast"/>
-                </div>
-            </div>
+    constructor(props){
+        super(props)
+        this.state = {
+            city: props.city!=null?props.city:null,
+            state: null,
+            image: null,
+            temperature: null,
+            feelsLike: null,
+            wind: null,
+            humidity: null
+        }
+        this.updateState = this.updateState.bind(this);
+        this.getWeather = this.getWeather.bind(this);
+    }
+    async getWeather(){
+        const API_key = "1195410e4ae2d05ce024d37aa18407de";
+        const url = "http://api.openweathermap.org/data/2.5/weather?q="+this.state.city+"&appid="+API_key;
+        //console.log(url);
+        const response = await axios.get(url).catch(async(error) =>{
+                console.log(error.toJSON());
+                return null;})
+        return response;
+    };
+    async updateState(){
+        //console.log("UpdateState!");
+        if(this.state.city === ""){
+            alert("Enter city");
+            return;
+        }
+        let weather = await this.getWeather();
     
-            <div className="forecastResult">
-                <ul>
-                    <li id="city"></li>
-                    <hr/>
-                    <li id="state"></li>
-                    <li><img id="image" alt=""/></li>
-                    <li id="temperature"></li>
-                    <li id="feelsLike"></li>
-                    <li id="wind"></li>
-                    <li id="humidity"></li>
-                </ul>
+        if(weather === null){
+            alert("Something went wrong!");
+            return;
+        }
+        console.log(weather);
+        this.setState(() => {
+            return{
+                image: "https://api.openweathermap.org/img/w/" + weather.data.weather[0].icon + ".png",
+                state:  weather.data.weather[0].description,
+                temperature: Math.round(weather.data.main.temp - 273.15),
+                feelsLike: Math.round(weather.data.main.temp - 273.15),
+                wind: Math.round(weather.data.wind.speed),
+                humidity: weather.data.main.humidity
+            }
+        });
+    }
+    render(){
+        return <div classNameName="App">
+        <section className="contentPanel">
+            <div className="gridContainer">
+                <div className="weatherForm">
+                    <h2>Enter <span>city</span> for the weather forecast</h2>
+                    <div id="WeatherContainer">
+                        <div className="formControl">
+                            <input id ="chosenCity" type="text" onChange={(e)=>this.setState({city: e.target.value})} value={this.state.city}/>
+                        </div>
+                        <input id ="submitBtn" className="forecastBtn" onClick={this.updateState} type="button" defaultValue="See forecast"/>
+                    </div>
+                </div>
+                {this.state.state != null&&
+                    <div className="forecastResult">
+                        <ul>
+                            <li>{this.state.city}</li>
+                            <hr/>
+                            <li>{this.state.state}</li>
+                            <li><img src= {this.state.image} alt=""/></li>
+                            <li>Temperature: {this.state.temperature}  °C</li>
+                            <li>Feels like: {this.state.feelsLike} °C</li>
+                            <li>Wind: {this.state.wind} m/s</li>
+                            <li>Humidity: {this.state.humidity}%</li>
+                        </ul>
+                    </div>
+                }
             </div>
+        </section>
         </div>
-    </section>
-    </div>
-  }
-}
-
+        }
+    }
 export default Weather;
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import Weather from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Weather/>
+    <Weather city="Astrakhan"/>
   </React.StrictMode>
+
 );
